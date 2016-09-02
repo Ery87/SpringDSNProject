19eed9ea9864fc50df04068958614f89222d5e74
Ery87 <Ery.ant87@gmail.com>
2016-09-02 11:58:44 +0200

    commit

---
+++ src/main/webapp/static/js/controller/generate_controller.js
@@ -0,0 +1,87 @@
+'use strict';
+
+App.controller('GenerateController',['$scope','$window','UserService',function($scope,$window,UserService){
+	var self=this;
+     var id;
+     var url='http://193.206.170.142/OSN';
+     //var url='http://localhost:8080/OSN';
+   
+   function readID(){
+       var url = window.location.pathname;
+       var id_u = url.substring(url.lastIndexOf('/') + 1);
+       return id_u;
+       }
+    
+   
+   $window.onload=function(){
+	   id=readID();
+	   
+   }
+   
+   self.reset=function(){
+       $scope.myForm.$setPristine(); //reset Form
+
+   }
+
+	self.generateKey=function(){
+		var Passphrase=$scope.ctrl.Passphrase;
+		var bits=1024;
+		var clientRSAKey=cryptico.generateRSAKey(Passphrase,bits);
+		var clientPublicKey=cryptico.publicKeyString(clientRSAKey);
+		
+		var keyPublic={"id":id,"key":clientPublicKey};
+			
+		UserService.savePKClient(keyPublic) //salvo la PKClient nel db
+			.then(
+					function(data){
+						UserService.getPKRMS()
+						.then(
+								function(data){
+									
+									var pkRMS=data.PKRMS;
+									console.log(pkRMS);
+									var encrypted=cryptico.encrypt(keyPublic,pkRMS);
+									var cipher=encrypted.cipher;
+									
+									var message={"id":id,"message":cipher};
+									
+								UserService.sendPKClient(message)
+							.then(
+									function(data){
+										console.log("ok");//key inviata correttamente
+									},function (errResponse){
+										console.error('Error while inviate pkIRMS');
+									});
+							},function(errResponse){
+							console.error('Error while search PKIRMS ');
+						});
+					},
+					function(errResponse){
+						console.error('Error while search PKIRMS on Server ');
+					});
+						
+					};
+		
+	
+
+
+
+}]);
+
+App.directive('fileModel', ['$parse', function ($parse) {
+	
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+
+}]);
